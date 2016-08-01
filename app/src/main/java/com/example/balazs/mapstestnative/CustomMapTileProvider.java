bc447b023e94094cee692771b1e68dff93e428b8
magyarb94@gmail.com <E6Xv5pvm>
2016-08-01 20:26:43 +0200

    init

---
+++ app/src/main/java/com/example/balazs/mapstestnative/CustomMapTileProvider.java
@@ -0,0 +1,74 @@
+package com.example.balazs.mapstestnative;
+
+import android.content.res.AssetManager;
+
+import com.google.android.gms.maps.model.Tile;
+import com.google.android.gms.maps.model.TileProvider;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by balazs on 2016. 07. 31..
+ */
+public class CustomMapTileProvider implements TileProvider {
+
+    private static final int TILE_WIDTH = 256;
+    private static final int TILE_HEIGHT = 256;
+    private static final int BUFFER_SIZE = 16 * 1024;
+
+    private AssetManager mAssets;
+
+    public CustomMapTileProvider(AssetManager assets) {
+        mAssets = assets;
+    }
+
+    @Override
+    public Tile getTile(int x, int y, int zoom) {
+        byte[] image = readTileImage(x, y, zoom);
+        return image == null ? null : new Tile(TILE_WIDTH, TILE_HEIGHT, image);
+    }
+
+    private byte[] readTileImage(int x, int y, int zoom) {
+        y = fixYCoordinate(y, zoom);
+        InputStream in = null;
+        ByteArrayOutputStream buffer = null;
+
+        try {
+            in = mAssets.open(getTileFilename(x, y, zoom));
+            buffer = new ByteArrayOutputStream();
+
+            int nRead;
+            byte[] data = new byte[BUFFER_SIZE];
+
+            while ((nRead = in.read(data, 0, BUFFER_SIZE)) != -1) {
+                buffer.write(data, 0, nRead);
+            }
+            buffer.flush();
+
+            return buffer.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        } catch (OutOfMemoryError e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (in != null) try { in.close(); } catch (Exception ignored) {}
+            if (buffer != null) try { buffer.close(); } catch (Exception ignored) {}
+        }
+    }
+
+    /**
+     * Fixing tile's y index (reversing order)
+     */
+    private int fixYCoordinate(int y, int zoom) {
+        int size = 1 << zoom; // size = 2^zoom
+        return size - 1 - y;
+    }
+
+    private String getTileFilename(int x, int y, int zoom) {
+        return "map/" + zoom + '/' + x + '/' + y + ".png";
+    }
+}
---
+++ app/src/main/java/com/example/balazs/mapstestnative/MapsActivity.java
@@ -0,0 +1,105 @@
+package com.example.balazs.mapstestnative;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+        mMap.setIndoorEnabled(false);
+        mMap.setBuildingsEnabled(false);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+        Location loc = mMap.getMyLocation();
+        mMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+            @Override
+            public boolean onMyLocationButtonClick() {
+                //Toast.makeText(getBaseContext(),String.valueOf(mMap.getMyLocation().getAccuracy()) ,Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
+            @Override
+            public void onMyLocationChange(Location location) {
+                //TODO - kidobni a masik activityt ha messze van vagy szar a vetel
+                Toast.makeText(getBaseContext(),String.valueOf(location.getAccuracy()) ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mMap.setOnCameraChangeListener(new GoogleMap.OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                float maxZoom = 22.0f;
+                float minZoom = 18.0f;
+                if (cameraPosition.zoom > maxZoom)
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(maxZoom));
+                if (cameraPosition.zoom < minZoom)
+                {
+                    mMap.animateCamera(CameraUpdateFactory.zoomTo(minZoom));
+                }
+            }
+        });
+
+        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(new CustomMapTileProvider(getResources().getAssets())));
+
+        CameraUpdate upd = CameraUpdateFactory.newLatLngZoom(new LatLng(47.164883, 20.182964), 18);
+        mMap.moveCamera(upd);
+        // Add a marker in Sydney and move the camera
+        //LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
